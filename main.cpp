0596780f831599952dbf7193381e0815214a9685
Hazem Massoud <hazem.massoud94@gmail.com>
2024-05-27 18:01:18 +0200

    Update main.cpp
    comment on how to optemize further

--- main.cpp
+++ main.cpp
@@ -27,7 +27,6 @@ int main() {
         if (a!= i && (res[a] == 0 || res[i] + 1 < res[a])) {
             res[a] = res[i] + 1;
             // go backwards starting from a and see if there's a new shortest path to arrive to intecections before (till the mid point between a and i)
-            // can be set to mid point intersection between a and the max between i and the last shortcut end as they can only move upwards
             for (int j = a-1; j >= (a+i)/2 && j > 1 ; j--) {
                 if (res[j] == 0 || res[j+1] + 1 < res[j]) {
                     res[j] = res[j+1] + 1;
@@ -36,6 +35,7 @@ int main() {
                 }
             }
         } 
+        // last if and loop can be removed by adding a queue of the shortcut ends, so that instead of going backwards step by step, we compare shortest path found (from previous intersection or a previous shortcut) with the shortest path found from at the next intersection with shortcut leading to (it plus diff between that intersection and current one)
 
     }
    
